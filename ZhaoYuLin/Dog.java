e1fb8b7adc152b52e9a0eb9ece1f31cbbb628bdb
Zhao <zlin20@stuy.edu>
2020-06-22 19:00:13 -0400

    fred good dog

--- ZhaoYuLin/Dog.java
+++ ZhaoYuLin/Dog.java
@@ -6,19 +6,50 @@ public class Dog {
     int age;
     boolean rabies;
 
-    public Dog(String name, String type, int age) {
+    public Dog(final String name, final String type, final int age) {
         this.name = name;
         breed = type;
         this.age = age;
         rabies = false;
     }
-   
+
     public void bark() {
         System.out.println("bork");
     }
-    
-    public static void main(String[] args) {
-        Dog foo = new Dog("foo", "golden retriever", 5);
+
+    public static void main(final String[] args) {
+        final Dog foo = new Dog("foo", "golden retriever", 5);
         foo.bark();
+        Dog Fred = new Dog();
+        Fred.bark();
+        System.out.println("Fred's age: " + Fred.getAge());
+        Fred.setRabies(true);
+        final Dog bar = new Dog("bar", "dachshund",1);
+        System.out.println("Bar's age in dog years: " + bar.getAgeinDogyears());
+        bar.setRabies(true);
+        System.out.println("Does bar have rabies: " + bar.hasRabies());
+
+
     }
+    public Dog(){
+        name = "Fred";
+        breed = "Husky";
+        age = 3;
+        rabies = true;
+        
+    
+    }
+    public int getAge(){
+        return age;
+    }
+    public int getAgeinDogyears(){
+        return (age + 7);
+    }
+    public boolean hasRabies(){
+        return rabies;
+    }
+    public void setRabies(boolean gotRabies){
+        rabies = gotRabies;
+    }
+
 }
